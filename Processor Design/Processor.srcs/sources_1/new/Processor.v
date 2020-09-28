2d0da78526fe8ceeafe1495522cdebeea15d0943
Jeffrey Blanda <41349027+Endeavor02@users.noreply.github.com>
2020-09-28 15:57:18 -0400

    Added registers

--- Processor Design/Processor.srcs/sources_1/new/Processor.v
+++ Processor Design/Processor.srcs/sources_1/new/Processor.v
@@ -1,4 +1,14 @@
-module Processor(
+module Processor(CLK, reset, IR, MD, AC, PC, MA);
+input CLK, reset;
+output reg [15:0]IR, MD, AC;
+output reg [11:0]PC, MA;
 
-    );
+
+
+//############################
+// Each clock cycle, execute commands.
+//############################
+always @ (posedge CLK) begin
+
+end
 endmodule
