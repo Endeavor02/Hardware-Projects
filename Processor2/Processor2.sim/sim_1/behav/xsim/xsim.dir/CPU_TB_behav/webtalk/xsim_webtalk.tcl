d97660d806df0c16039946e8b9313a5a7d1a71ab
Jeffrey Blanda <41349027+Endeavor02@users.noreply.github.com>
2020-12-03 00:14:06 -0500

    Tweaked the waveform configuration

--- Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/xsim_webtalk.tcl
+++ Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/jblan/Documents/GitHub/Hardware-Projects/Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Nov 30 18:39:12 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Dec  3 00:13:52 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2902540" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "227be982-aea3-412b-aae4-47490239fc98" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "ffe16b907ee14f37a56995170d36612f" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "151" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "155" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 3600X 6-Core Processor             " -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "98 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6192_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3330595992 -regid "" -xml C:/Users/jblan/Documents/GitHub/Hardware-Projects/Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/jblan/Documents/GitHub/Hardware-Projects/Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/jblan/Documents/GitHub/Hardware-Projects/Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6056_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2832899810 -regid "" -xml C:/Users/jblan/Documents/GitHub/Hardware-Projects/Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/jblan/Documents/GitHub/Hardware-Projects/Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/jblan/Documents/GitHub/Hardware-Projects/Processor2/Processor2.sim/sim_1/behav/xsim/xsim.dir/CPU_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
